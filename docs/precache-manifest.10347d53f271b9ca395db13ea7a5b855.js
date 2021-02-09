77467772e7e5aad50d06a0801db810925816e7c8
morel <byox4329@gmail.com>
2021-02-09 14:55:07 +0100

    Clean app; Add Test

---
+++ docs/precache-manifest.10347d53f271b9ca395db13ea7a5b855.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f035c2cb5aae780ef6",
+    "url": "/vue-app-2/css/app.d301cb46.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "fbb519266b2b76cc78dd9e8cb8db0250",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "3deac8955d7f1bf6db93",
+    "url": "/vue-app-2/js/about.1287ecd9.js"
+  },
+  {
+    "revision": "04f035c2cb5aae780ef6",
+    "url": "/vue-app-2/js/app.e98396bc.js"
+  },
+  {
+    "revision": "f28e1fe54cbb3ca988a4",
+    "url": "/vue-app-2/js/chunk-vendors.7e47f066.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
